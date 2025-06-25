2e85384354dbbc18bd5378c27329ea92ad24b9a0
Roy <Roy@Admins-MacBook-Pro.local>
2025-06-25 11:54:47 +0200

    underline footer

--- components/footer.php
+++ components/footer.php
@@ -2,12 +2,8 @@
 
     <section class="text-white p-4 border-t border-gray-700 w-full bg-lightblack flex items-center justify-between">
         <h3 class="font-bold">Copyright <span class="font-bold text-tailwindblue">©</span> 2025 <span class="font-bold text-tailwindblue">-</span> <span class="transition-opacity duration-500 cursor-pointer" id="wave" onclick="wavefade(this)">Roy Aardenburg</span></h3>
-        <a href="/404">404</a>
+        <a class="hover:underline" href="/404">404</a>
     </section>
-
-    <script>
-
-    </script>
     </body>
 
     </html>
